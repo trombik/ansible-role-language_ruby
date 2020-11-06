7b89e66bb2cf32e7d64c7430c5d07430ef0f5d05
Tomoyuki Sakurai <y@trombik.org>
2020-11-06 14:01:58 +0900

    bugfix: ruby24 is dead in FreeBSD ports, moved to ruby26

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -6,7 +6,7 @@ package = "ruby"
 
 case os[:family]
 when "freebsd"
-  package = "lang/ruby24"
+  package = "lang/ruby26"
 when "ubuntu"
   if host_inventory["platform_version"].to_f == 16.04
     package = "ruby2.3"
