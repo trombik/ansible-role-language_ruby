242a0666791705310df1ed567ff7d7d5f7a73414
Tomoyuki Sakurai <y@trombik.org>
2022-05-08 10:32:00 +0700

    update: drop Ubuntu 18, update to Devuan 4, OpenBSD 7.0

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -6,7 +6,7 @@ package = "ruby"
 
 case os[:family]
 when "freebsd"
-  package = "lang/ruby26"
+  package = "lang/ruby27"
 when "ubuntu"
   if host_inventory["platform_version"].to_f == 16.04
     package = "ruby2.3"
